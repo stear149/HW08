a6810344e32ea2200380defab0c3c56a88269472
Owen-Le-Coder <haber297@umn.edu>
2025-10-29 14:36:24 -0500

    Update computeVdot.m

--- computeVdot.m
+++ computeVdot.m
@@ -1,3 +1,44 @@
+%==========================================================================
+% function [dv] = computeVdot(t, V, r, L)
+%
+% Input Arguments:
+% t is the time since the beginning of the rainfall event. t is a scalar. 
+% t has units of [s]
+%
+% V is the volume of water in the ponds. V is a (2 × 1) matrix: 
+% V has units of [m3]
+%
+% r is a (3 x 1) matrix containing ra, rb, and rc
+% ra is the base radius of the first pond. ra is a scalar. 
+% ra has units of [m]
+%
+% rb is the base radius of the second pond. rb is a scalar. 
+% rb has units of [m]
+%
+% rc is the base radius of the second pond. rb is a scalar. 
+% rc has units of [m]
+%
+% L is a (3 x 1) matrix containing La, Lb, and Lc
+% La is the length of the weir in the first pond. La is a scalar. 
+% La has units of [m]
+%
+% Lb is the length of the weir in the second pond. Lb is a scalar. 
+% Lb has units of [m]
+%
+% Lc is the length of the weir in the second pond. Lb is a scalar. 
+% Lc has units of [m]
+%
+% Returns:
+% dVdt is the first derivatives of the pond volumes with respect to time 
+% dVdt is a (3 × 1) matrix: 
+% dVdt(1) is the derivative for the first pond
+% dVdt(2) is the derivative for the second pond. dVdt has units of [m^3/s]
+% dVdt(3) is the derivative for the third pond. dVdt has units of [m^3/s]
+%
+% Author: Group I
+%
+% Version 29 Oct. 2025
+%==========================================================================
 function [dv] = computeVdot(t, V, r, L)
     
     Qin = computeQin(t);
