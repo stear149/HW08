8d36bba43b32db5d031f59abf2e05f9105b14efd
Owen-Le-Coder <haber297@umn.edu>
2025-10-29 14:29:11 -0500

    Update computeQout.m

--- computeQout.m
+++ computeQout.m
@@ -1,3 +1,19 @@
+%==========================================================================
+% function[Q] = computeQout(d, L)
+%
+% Input Arguments:
+% d is the depth of water in the pond. d is a scalar. d has units of [m]
+% L is the length of the weir in the pond. L is a scalar. 
+% L has units of [m]
+%
+% Returns:
+% Q is the outflow discharge from the pond at depth d. Q is a scalar. 
+% Q has units of [m^3/s]
+%
+% Author: Group I
+%
+% Version 29 Oct. 2025
+%==========================================================================
 function[Q] = computeQout(d, L)
     C = 1.84;
     if d <= 1
